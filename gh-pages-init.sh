84b98e376ec5a0ca700c06440fad808202234d93
Shuguang Sun <shuguang79@qq.com>
2020-05-16 20:14:59 +0800

    fix(misc): add upstream

--- gh-pages-init.sh
+++ gh-pages-init.sh
@@ -1,3 +1,4 @@
+## git remote add upstream git@github.com:ShuguangSun/PoorStatistician.git
 git checkout --orphan gh-pages
 git reset --hard
 git commit --allow-empty -m "Initializing gh-pages branch"
