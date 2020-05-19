f4bd2f92be30404c0ebc2c385579d02470182baa
Shuguang Sun <shuguang79@qq.com>
2020-05-19 15:39:09 +0800

    add(dir-locals): time-stamp

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((markdown-mode . ((time-stamp-format . "%Y-%02m-%02dT%02H:%02M:%02S")
+                   (time-stamp-end . "+08:00$")
+                   (time-stamp-line-limit . 15)
+                   (time-stamp-start . "lastmod:[ 	]+"))))
